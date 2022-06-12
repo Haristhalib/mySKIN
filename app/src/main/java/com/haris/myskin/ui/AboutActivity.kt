26f25bff4dad844235df3ef2b4798b75f5bf4b43
Reza <55218582+RwD31@users.noreply.github.com>
2022-06-12 09:23:03 +0700

    giving contents to each product activity , fixing bottom navigation ( now working on every page) , set title app bar name for each activity

--- app/src/main/java/com/haris/myskin/ui/AboutActivity.kt
+++ app/src/main/java/com/haris/myskin/ui/AboutActivity.kt
@@ -1,12 +1,75 @@
 package com.haris.myskin.ui
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.haris.myskin.R
+import com.haris.myskin.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAboutBinding
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == NewsActivity.REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+    private fun allPermissionsGranted() = NewsActivity.REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setTitle(R.string.about2)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                NewsActivity.REQUIRED_PERMISSIONS,
+                NewsActivity.REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.btnHlp.setOnClickListener {
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnAbt.setOnClickListener {
+            val intent = Intent(this, DetailAboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.bthome.setOnClickListener {
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btscan.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btproduct.setOnClickListener {
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
